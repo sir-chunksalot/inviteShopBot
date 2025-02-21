11ea15cf916dd6e738c9868ba3f983a34e9df596
sir-chunksalot <knight1130@icloud.com>
2025-02-20 23:58:53 -0700

    day 1 progress
    basic tracking of invites, set up files for storing invites and shop codes, set up the basics for the main commands

--- main/main.py
+++ main/main.py
@@ -1,10 +1,11 @@
 import discord
 import discord.sku
+import os
 
 token = "MTM0MjMzNzU4NDg2MDIzMzc4OA.G8p8L3.VR_CUD8OPccD6zWdaRaWxPhcCP4kTWp5lSJlJY"
 
 client = discord.Client()
-
+INVITE_FILE = 'main/invites.txt'
 
 
 @client.event
@@ -14,7 +15,24 @@ async def on_ready():
 @client.event
 async def on_message(message): 
     if message.content.startswith('$shop'):
-       await message.channel.send('shop accessed')
+        try:
+            with open('main/nitro_codes.txt', 'r') as file:
+                lines = file.readlines()
+                
+            shop_message = "Welcome to the Nitro Shop! Here's what we have:\n\n"
+            count = 0
+            for line in lines:
+                count += 1
+                if count == 1: 
+                    continue
+                parts = line.split(" | ")
+                subscription_type = parts[0] 
+                cost = parts[1]
+                shop_message += f"{subscription_type}: `{cost}`\n"
+            await message.channel.send(shop_message)
+            
+        except Exception as e:
+            await message.channel.send(f"An error occurred while reading the file: {e}")
 
     if message.content.startswith('$invite'):
         channel = message.channel
@@ -30,6 +48,31 @@ async def on_message(message):
                 count += invite.uses
                 
         await message.channel.send(f'This user has sent {count} invite(s)')
+    
+    if message.content.startswith('$leaderboard'):
+        try:
+            with open(INVITE_FILE, 'r') as file:
+                lines = file.readlines()
+            leaderboard_message = 'Lifetime Invite Leaderboard\n\n'
+            
+            for line in lines:
+                parts = line.split(" | ")
+                username = parts[0]
+                lifetime_invites = [1]
+                current_cash = [2]
+                leaderboard_message += f"`{username}`: Lifetime Invites\n"
+        except Exception as e:
+             await message.channel.send(f"An error occurred while reading the file: {e}")
+            
+    if message.content.startswith('$debug'): #REMOVE BEFORE PUBLISHING
+        print(os.listdir())
+        
+@client.event
+async def on_member_join(ctx):
+    #use new_member to verfiy its a legit invite
+    #url to find which link it was 
+    invites = await ctx.channel.invites()
+
             
 
 
